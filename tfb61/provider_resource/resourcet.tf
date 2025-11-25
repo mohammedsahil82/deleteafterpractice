2734eee14e95bbf04b6284928f1f729277e97936
Mohammed Sahil <107857213+mohammedsahil82@users.noreply.github.com>
2025-11-26 00:41:17 +0530

    Update resourcet.tf

--- tfb61/provider_resource/resourcet.tf
+++ tfb61/provider_resource/resourcet.tf
@@ -2,6 +2,7 @@ resource "aws_instance" "example" {
    
    instance_type = "t3.micro"
    ami = "ami-0fa3fe0fa7920f68e"
-    key_name = "30july"
-   vpc_security_group_ids = [ "sg-0bbfe8e7d4bf3c179" ]
+    key_name = "kubernetes"
+   vpc_security_group_ids = [ "sg-0c532b3818fa042a4" ]
+
 }
